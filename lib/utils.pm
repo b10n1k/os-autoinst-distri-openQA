ea6dc6d6c0bf0dc2474c0064cfb4f2cccafb95ab
ybonatakis <ybonatakis@suse.com>
2024-01-17 12:54:20 +0100

    breaking changes
    Signed-off-by: ybonatakis <ybonatakis@suse.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -33,7 +33,7 @@ sub handle_gui_password {
 }
 
 sub wait_for_desktop {
-    assert_screen([qw/boot-menu openqa-desktop/]);
+    assert_screen([qw/bootmenu openqa-desktop/]);
     send_key 'ret' if match_has_tag('boot-menu');
     for (1..3) {
         assert_screen 'openqa-desktop', 500;
